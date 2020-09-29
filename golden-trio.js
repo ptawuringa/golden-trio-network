7d5136dfd889ca001f0c60fb52cd5a91de9288df
Cebi-coder <cebisapeter40@gmail.com>
2020-09-29 12:52:48 +0200

    first commit

---
+++ golden-trio.js
@@ -0,0 +1,5 @@
+const businessOwnerRadio = document.querySelector(".servicesProviderRadio");
+const clientRadio = document.querySelector(".clientRadio");
+const callTotalRadio = document.querySelector(".callTotalTwo");
+const nextBtnElem = document.querySelector(".nextBtn");
+
